1892784f04573cdbede9b1fdfde24b4482dc77ac
tolya <tolya@gmx.de>
2024-11-19 15:46:22 +0100

    добавлены интерфейс и имплементация UserService

---
+++ src/main/java/project/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package project.service;
+
+import project.entity.User;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public List<User> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public void create(User user) {
+
+    }
+
+    @Override
+    public User getById(long id) {
+        return null;
+    }
+
+    @Override
+    public void remove(long id) {
+
+    }
+}
