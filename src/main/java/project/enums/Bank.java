372a7d82e1da161a85ee85a6856677329a9de73e
tolya <tolya@gmx.de>
2024-11-19 21:49:19 +0100

    добавил энумы Bank и Currency

---
+++ src/main/java/project/enums/Bank.java
@@ -0,0 +1,13 @@
+package project.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum Bank {
+
+    Lombard_Odier("Swiss"),
+    HSBC("England"),
+    La_Banque_Postale("France"),;
+
+    private String description;
+}
---
+++ src/main/java/project/enums/Currency.java
@@ -0,0 +1,17 @@
+package project.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum Currency {
+
+    DOLLAR("USA"),
+    EURO("Europe"),
+    POUND_STERLING("United Kingdom"),
+    SWISS_FRANC("Swiss"),
+    YEN("Japan"),
+    YUAN("China"),
+    RUBLES("Russia");
+
+    private String description;
+}
